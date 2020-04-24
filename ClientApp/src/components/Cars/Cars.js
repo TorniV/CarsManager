c4939424d7013a43b931bcbbd2380b42525e7be0
Patryk Suchnaek <patryksuchanek@gmail.com>
2020-04-24 20:54:51 +0200

    Implement working routing for Cars tab

--- ClientApp/src/components/Cars/Cars.js
+++ ClientApp/src/components/Cars/Cars.js
@@ -1,10 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
-  useParams,
   useRouteMatch,
 } from "react-router-dom";
 import { SideNav } from "./SideNav/SideNav";
@@ -13,40 +11,25 @@ import { Fuel } from "./Pages/Fuel";
 import { Services } from "./Pages/Services";
 import { Insurance } from "./Pages/Insurance";
 
-export class Cars extends Component {
-  render() {
-    // const match = useRouteMatch();
-    // let { path, url } = useRouteMatch();
-    return (
-      <div className="cars-page">
-        <SideNav />
-        <Switch>
-          <Route path="/fuel">
-            <Fuel />
-          </Route>
-          <Route path="/services">
-            <Services />
-          </Route>
-          <Route path="/insurance">
-            <Insurance />
-          </Route>
-          <Route path="/">
-            <DashBoard />
-          </Route>
-          {/* <Route path={"${path}/fuel"}>
-            <Fuel />
-          </Route>
-          <Route path={"${path}/services"}>
-            <Services />
-          </Route>
-          <Route path={"${path}/insurance"}>
-            <Insurance />
-          </Route>
-          <Route exact path={path}>
-            <DashBoard />
-          </Route> */}
-        </Switch>
-      </div>
-    );
-  }
+export function Cars() {
+  let { path } = useRouteMatch();
+  return (
+    <div className="cars-page">
+      <SideNav />
+      <Switch>
+        <Route path={`${path}/fuel`}>
+          <Fuel />
+        </Route>
+        <Route path={`${path}/services`}>
+          <Services />
+        </Route>
+        <Route path={`${path}/insurance`}>
+          <Insurance />
+        </Route>
+        <Route exact path={path}>
+          <DashBoard />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
--- ClientApp/src/components/Cars/SideNav/SideNav.js
+++ ClientApp/src/components/Cars/SideNav/SideNav.js
@@ -1,26 +1,25 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
-export class SideNav extends Component {
-  render() {
-    return (
-      <div className="side-nav">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/fuel">Tankowanie</Link>
-            </li>
-            <li>
-              <Link to="/services">Serwisowanie</Link>
-            </li>
-            <li>
-              <Link to="/insurance">Ubezpieczenie i przeglądy</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    );
-  }
+import React from "react";
+import { BrowserRouter as Router, Link, useRouteMatch } from "react-router-dom";
+export function SideNav() {
+  let { url } = useRouteMatch();
+  return (
+    <div className="side-nav">
+      <nav>
+        <ul>
+          <li>
+            <Link to={`${url}`}>Home</Link>
+          </li>
+          <li>
+            <Link to={`${url}/fuel`}>Tankowanie</Link>
+          </li>
+          <li>
+            <Link to={`${url}/services`}>Serwisowanie</Link>
+          </li>
+          <li>
+            <Link to={`${url}/insurance`}>Ubezpieczenie i przeglądy</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
