794aded5221cb040139d952743133c440a7d7e70
Patryk Suchnaek <patryksuchanek@gmail.com>
2021-01-25 19:24:40 +0100

    Updates

---
+++ precache-manifest.fa124819709de74f959861eb59712a81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa99cf324911e06a5adffac3b2f0df4",
+    "url": "/CarsManager/index.html"
+  },
+  {
+    "revision": "cd7534554b7332453869",
+    "url": "/CarsManager/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "618a80379c61b29dadd6",
+    "url": "/CarsManager/static/css/main.64d1b2bc.chunk.css"
+  },
+  {
+    "revision": "cd7534554b7332453869",
+    "url": "/CarsManager/static/js/2.a63a74f4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/CarsManager/static/js/2.a63a74f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "618a80379c61b29dadd6",
+    "url": "/CarsManager/static/js/main.70aa375d.chunk.js"
+  },
+  {
+    "revision": "54eac8b705ea08fea3f4",
+    "url": "/CarsManager/static/js/runtime-main.7b43ca16.js"
+  }
+]);
